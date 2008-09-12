4e29551e9f9949a141923b4e4abade92c67c4ed7
Fabian Linzberger <fabian.linzberger@mm-karton.com>
2008-09-12 15:40:16 +0200

    add initial version of dot-debs to list debian packages i always want installed

--- dot-bin/wget-slurp.sh
+++ dot-bin/wget-slurp.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-wget --recursive --level=5 --convert-links
+wget --recursive --convert-links --page-requisites $@
 
 # --span-hosts
 # --relative
 # --no-parent
+# --level=5
+# --include-directories=a,b
 
+# mmwiki hack
+#find . -type f -exec sed -i "s/url('\/twiki/url('..\/..\/twiki/" {} \;
