35baa807ddb18817fff9d5372db31ca3b54482c1
Fabian Linzberger <fabian@klarna.com>
2010-05-12 10:10:30 +0200

    update emacs erlang config distel stuff

--- deep-emacs.d/erlang-conf.el
+++ deep-emacs.d/erlang-conf.el
@@ -1,11 +1,11 @@
 ;; This is needed for Erlang mode setup
 (setq erlang-root-dir "/usr/lib/erlang")
-(setq load-path (cons "/usr/lib/erlang/lib/tools-2.6.1/emacs" load-path))
+(setq load-path (cons "/usr/lib/erlang/lib/tools-2.6.5/emacs" load-path))
 (setq exec-path (cons "/usr/lib/erlang/bin" exec-path))
 (require 'erlang-start)
 
 ;; This is needed for Distel setup
-(let ((distel-dir "/home/lefant/svn/distel/elisp"))
+(let ((distel-dir "/home/lefant/git/distel/elisp"))
   (unless (member distel-dir load-path)
     ;; Add distel-dir to the end of load-path
     (setq load-path (append load-path (list distel-dir)))))
