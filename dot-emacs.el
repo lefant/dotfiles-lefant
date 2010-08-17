be0f6cccd687a081515ab1d5f368fb7237dcb2d5
Fabian Linzberger <fabian@klarna.com>
2010-08-17 13:38:05 +0200

    better ssh config

--- dot-emacs.el
+++ dot-emacs.el
@@ -2,3 +2,18 @@
 ;; aptitude install darcs screen zsh lftp rsync
 
 (load "~/.emacs.d/emacs.el")
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(haskell-program-name "ghci")
+)
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+
+(put 'downcase-region 'disabled nil)
