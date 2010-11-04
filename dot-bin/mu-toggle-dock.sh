45fc5532bc6227593524f4bd59f50bb6e42010cc
Fabian Linzberger <e@lefant.net>
2010-11-04 08:12:58 +0100

    mu dock/undock quickhack script

---
+++ dot-bin/mu-toggle-dock.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ $# -eq 1 ]
+then
+    case $1 in
+        dock)
+            xrandr --auto --output LVDS1 --off
+            rm ~/.xmonad/xmonad.hs
+            ln -s ~/.xmonad/xmonad-mu-diamond.hs ~/.xmonad/xmonad.hs
+            xmonad --recompile
+            xmonad --restart
+            ;;
+        launch)
+            xrandr --auto --output VGA1 --off
+            rm ~/.xmonad/xmonad.hs
+            ln -s ~/.xmonad/xmonad-mu-ctrl-alt.hs ~/.xmonad/xmonad.hs
+            xmonad --recompile
+            xmonad --restart
+            ;;
+    esac
+else
+    echo "USAGE:"
+    echo "$0 dock"
+    echo "$0 launch"
+fi
