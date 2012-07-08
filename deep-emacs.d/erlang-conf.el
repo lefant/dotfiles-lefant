9446a479845be471265dc8de2794b92a34a12e6e
Fabian Linzberger <e@lefant.net>
2012-07-08 14:20:01 +0200

    formatting in erlang-conf.el

--- deep-emacs.d/erlang-conf.el
+++ deep-emacs.d/erlang-conf.el
@@ -1,3 +1,4 @@
+;; not yet in melpa / marmelade
 (add-to-list 'load-path "~/git/other/distel/elisp")
 (require 'distel)
 (distel-setup)
@@ -7,11 +8,13 @@
 (require 'flymake)
 (defun flymake-erlang-init ()
   (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                          'flymake-create-temp-inplace))
-          (local-file (file-relative-name temp-file
-                                          (file-name-directory buffer-file-name))))
+                     'flymake-create-temp-inplace))
+         (local-file
+          (file-relative-name temp-file
+                              (file-name-directory buffer-file-name))))
     (list "~/.emacs.d/eflymake.escript" (list local-file))))
 
-(add-to-list 'flymake-allowed-file-name-masks '("\\.erl\\'" flymake-erlang-init))
+(add-to-list 'flymake-allowed-file-name-masks
+             '("\\.erl\\'" flymake-erlang-init))
 
 (add-hook 'erlang-mode-hook 'flymake-mode)
