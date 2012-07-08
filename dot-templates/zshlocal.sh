77d59b3cc7dc3bca6cf9c7140e3bcf4410734327
Fabian Linzberger <e@lefant.net>
2012-07-08 14:17:59 +0200

    update zshlocal.sh template

--- dot-templates/zshlocal.sh
+++ dot-templates/zshlocal.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/zsh
 
-export LANG="en_US.utf8"
+## mac stuff
+#alias emacs=/Applications/Emacs.app/Contents/MacOS/Emacs
+#alias emacsclient=/Applications/Emacs.app/Contents/MacOS/bin/emacsclient
+#find_best_editor () {
+#    export EDITOR="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -t"
+#    export VISUAL=$EDITOR
+#    alias e="$EDITOR"
+#}
 
-#export TERM="screen-256color"
+# needed for mosh!
+#export LANG="en_US.UTF-8"
+#export LC_CTYPE="en_US.UTF-8"
 
+
+# tmux
+#export SHELL=/bin/zsh
+#export PATH="$HOME/Library/Haskell/bin:/usr/local/bin:$PATH"
