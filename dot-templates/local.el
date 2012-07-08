e1a34f050c6b010ea0b2583cb71d517ae3940073
Fabian Linzberger <e@lefant.net>
2012-07-08 14:18:46 +0200

    cleanup local.el template

--- dot-templates/local.el
+++ dot-templates/local.el
@@ -3,50 +3,12 @@
 (require 'edit-server)
 (edit-server-start)
 
-;;(load "w3m-conf.el")
-
 (setq browse-url-browser-function (quote browse-url-generic))
 (setq browse-url-generic-program "chromium-browser")
 
-(add-to-list 'load-path "/home/lefant/git/git-emacs")
-;;(require 'git-emacs)
+(load "elpa-conf.el")
 
-;; mac os x erlang install path
-;(add-to-list 'load-path "/usr/lib/erlang/lib/tools-2.6.6.6/emacs")
-;(load "erlang-conf.el")
+(load "erlang-conf.el")
+(setq erlang-indent-level '4)
 
-;(require 'magit)
-
-;(load "puppet-conf.el")
-
-
-;;(require 'org)
-;;(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-;;(global-set-key "\C-cl" 'org-store-link)
-;;(global-set-key "\C-ca" 'org-agenda)
-
-;;(load "bbdb-conf.el")
-;;(load "auctex-conf.el")
-
-;;(load "erc-conf.el")
-
-;;(load "cperl-conf.el")
-;;(load "perly-sense-conf.el")
-
-;;(load "scheme-conf.el")
-
-;;(require 'color-theme)
-;;(color-theme-subtle-hacker)
-
-
-;;(load "gnus-conf.el")
-;;(load "gnus-conf-mm.el")
-
-
-
-;;(load "erc-conf-work.el")
-
-
-;; (add-to-list 'load-path "/home/lefant/shared/code/erlang/distel/elisp")
-;; (require 'distel)
-;; (distel-setup)
+(load "haskell-conf.el")
