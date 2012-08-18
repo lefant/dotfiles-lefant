f92c2d24a6879d3f6aa48332e6d24fdf13d0ce1f
Fabian Linzberger <e@lefant.net>
2012-08-18 13:48:30 +0200

    improve emacs haskell config

--- deep-emacs.d/haskell-conf.el
+++ deep-emacs.d/haskell-conf.el
@@ -11,7 +11,14 @@
 
 
 
+;; haskell-mode
+;; git clone git://github.com/haskell/haskell-mode.git
+(add-to-list 'load-path "~/git/other/haskell-mode")
+(load "~/git/other/haskell-mode/haskell-site-file")
+
 (custom-set-variables '(haskell-program-name "ghci"))
+(custom-set-variables '(haskell-tags-on-save t))
+(custom-set-variables '(haskell-stylish-on-save t))
 
 (add-hook 'haskell-mode-hook
           (lambda ()
@@ -20,5 +27,6 @@
             (turn-on-haskell-indent)
             (turn-on-haskell-font-lock)
             (turn-on-haskell-decl-scan)
+            (define-key haskell-mode-map (kbd "C-x C-s") 'haskell-mode-save-buffer)
             (ghc-init)
             (flymake-mode)))
