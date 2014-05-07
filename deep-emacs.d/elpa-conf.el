9d37dec1a5dc48386a7eeb5a1d7f414cb5f2dc0a
Fabian Linzberger <e@lefant.net>
2014-05-07 06:41:14 +0200

    use melpa for emacs packages

--- deep-emacs.d/elpa-conf.el
+++ deep-emacs.d/elpa-conf.el
@@ -1,8 +1,6 @@
 (require 'package)
 
-;;(add-to-list 'package-archives
-;;             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (package-initialize)
