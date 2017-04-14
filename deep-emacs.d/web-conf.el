447d727b32c78791913de0eca5e391b13db9780f
Fabian Linzberger <e@lefant.net>
2017-04-14 13:12:59 +0200

    use eslint-fix in emacs web-mode

--- deep-emacs.d/web-conf.el
+++ deep-emacs.d/web-conf.el
@@ -1,16 +1,25 @@
-
 ;; as described here
 ;; http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
 
-;; npm install -g eslint babel-eslint eslint-plugin-react
+;; package-list-packages
+;; web-mode flycheck eslint-fix
+
+;; npm install -g yarn
+;; yarn global add eslint prettier eslint-plugin-prettier standard eslint-config-standard eslint-config-standard-babel eslint-config-standard-react
+
+;; .eslintrc.js
+;; module.exports = {
+;;   parser: 'babel-eslint',
+;;   extends: ['standard', 'standard-babel', 'standard-react'],
+;;   plugins: ['babel', 'prettier', 'standard']
+;; }
+
 
 ;; use web-mode for .jsx files
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js$" . web-mode))
 
-;; http://www.flycheck.org/manual/latest/index.html
 (require 'flycheck)
-
 ;; turn on flychecking globally
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
@@ -29,12 +38,7 @@
   (exec-path-from-shell-initialize))
 
 
-;; adjust indents for web-mode to 2 spaces
 (defun my-web-mode-hook ()
-  "Hooks for Web mode. Adjust indents"
-    ;;; http://web-mode.org/
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-  (electric-indent-local-mode -1))
+  (add-hook 'before-save-hook 'eslint-fix nil t))
 (add-hook 'web-mode-hook 'my-web-mode-hook)
+
