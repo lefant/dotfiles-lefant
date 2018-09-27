0203031d90d0fbf7ea6dbfc55dbe5de3e13c5821
Fabian Linzberger <e@lefant.net>
2018-09-27 14:06:36 +0200

    cleanup zshuser-lefant.sh

--- deep-zsh/zshuser-lefant.sh
+++ deep-zsh/zshuser-lefant.sh
@@ -16,7 +16,7 @@ alias cpan_modulebuild_style_install="perl Build.PL; ./Build; ./Build test && su
 [[ ! -e ~/.zsh/zshlocal.sh ]] && cp -v ~/.templates/zshlocal.sh ~/.zsh/zshlocal.sh
 [[ -e ~/.emacs.d ]] && [[ ! -e ~/.emacs.d/local.el ]] && cp -v ~/.templates/local.el ~/.emacs.d/local.el
 
-for dir in ~/.ssh/sock ~/.encfs ~/.maybe_krb5ccnamesh.d
+for dir in ~/.ssh/sock ~/.encfs
 do
     [[ ! -d $dir ]] && mkdir -p $dir
 done
@@ -34,54 +34,13 @@ find_best_editor () {
     alias e="$EDITOR"
 }
 
-maybe_krb5ccnamesh () {
-    krb5ccnamesh_file=~/.maybe_krb5ccnamesh.d/${HOST}.sh
-    if [ `klist --test &>/dev/null ; echo $?` = 0 -a ! -z $KRB5CCNAME ]
-    then
-        echo "export KRB5CCNAME=$KRB5CCNAME" >$krb5ccnamesh_file
-    elif [ `klist --test &>/dev/null ; echo $?` = 1 -a -f $krb5ccnamesh_file ]
-    then
-        source $krb5ccnamesh_file
-        [ -f /usr/bin/aklog ] && aklog
-        if [ ! `klist --test &>/dev/null ; echo $?` = 0 ]
-        then
-            rm $krb5ccnamesh_file
-            unset KRB5CCNAME
-        fi
-    fi
-}
-
-maybe_source_keychain () {
-    if [ `ssh-add -l &>/dev/null ; echo $?` = 2 -a -f $HOME/.keychain/$HOST-sh ]
-    then
-        source $HOME/.keychain/$HOST-sh
-        if [ ! `ssh-add -l &>/dev/null ; echo $?` = 0 ]
-        then
-            rm -r $HOME/.keychain
-            killall ssh-agent
-            unset SSH_AUTH_SOCK
-        fi
-    fi
-}
-
 fix_env () {
-    maybe_source_keychain
-    maybe_krb5ccnamesh
     find_best_editor
 
     watch=(notme)
     #setopt notify
 }
 
-maybe_run_keychain () {
-    if [ -e /usr/bin/keychain ]
-    then
-        if [ `ssh-add -l &>/dev/null ; echo $?` = 0 -a ! -z $SSH_AUTH_SOCK ]
-        then
-            keychain -q --inherit any
-        fi
-    fi
-}
 
 # set screen / tmux window title to hostname / sudo command
 cool_window_title() {
@@ -117,7 +76,6 @@ case $TMUX in
         }
         ;;
     *)
-        maybe_run_keychain
         fix_env
 
         if [ -z "$SSH_TTY" -a -z "$DOCKER_HACKBOX" ]; then
@@ -132,40 +90,13 @@ case $TMUX in
             esac
 
             unsetopt notify
-            #unison -silent -terse &>/dev/null
-
-#            if touch /dev/fuse &>/dev/null
-#            then
-#                if [ -e /usr/bin/afuse ]
-#                then
-#                    afuse \
-#                        -o mount_template="sshfs -o reconnect %r:/ %m" \
-#                        -o unmount_template="fusermount -u -z %m" \
-#                        ~/sshfs/ &
-#                fi
-#
-#                if [ -e /usr/bin/encfs ]
-#                then
-#                    afuse \
-#                        -o mount_template='sh -c "encfs -S /home/.enc/%r %m <~/.secret/encfs/%r"' \
-#                        -o unmount_template="fusermount -u -z %m" \
-#                        ~/.encfs
-#                fi
-#            fi
 
             tmux -L meta -f ~/.tmux.conf.meta attach -t meta
-
-            # # run zeiterfassung end
-	    # cd ~/shared/arbeitszeit/`hostname`
-	    # echo "work" |~/shared/code/python/timelog/timelog.py
 
             mount |grep -q ~/.encfs && fusermount -u -z ~/.encfs
 
             which kdestroy >/dev/null && kdestroy -q &>/dev/null
 
-            #unison -silent -terse &
-            #disown
-            #exit 0
         else
             tmux -L default attach
         fi
