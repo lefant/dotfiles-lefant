bdcf3ca5b7c3c66a99a3c818f3257637fed59948
Fabian Linzberger <e@lefant.net>
2018-10-26 09:43:27 +0200

    add emacs java-conf.el

---
+++ deep-emacs.d/java-conf.el
@@ -0,0 +1,13 @@
+(require 'meghanada)
+(add-hook 'java-mode-hook
+          (lambda ()
+            ;; meghanada-mode on
+            (meghanada-mode t)
+            (flycheck-mode +1)
+            (setq c-basic-offset 2)
+            ;; use code format
+            (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
+
+(setq meghanada-java-path "java")
+(setq meghanada-maven-path "mvn")
+(setq meghanada-gradle-path "gradle")
