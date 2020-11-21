69e2ff5de4d40c50040bb790b22d3c3972d10b6d
Fabian Linzberger <e@lefant.net>
2020-11-21 16:11:45 +0100

    refresh emacs python-conf

--- deep-emacs.d/python-conf.el
+++ deep-emacs.d/python-conf.el
@@ -1,3 +1,10 @@
-(add-hook 'python-mode-hook 'yapf-mode)
+(add-hook 'python-mode-hook 'blacken-mode)
+
+(pyenv-mode)
+(require 'pyenv-mode-auto)
 
 (elpy-enable)
+
+(require 'python)
+(setq python-shell-interpreter "ipython")
+
