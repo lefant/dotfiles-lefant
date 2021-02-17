07bea9afd8548976d5a28a25407ade8c0a76486d
Fabian Linzberger <e@lefant.net>
2021-02-17 10:09:08 +0100

    snapshot dot-emacs.el

--- dot-emacs.el
+++ dot-emacs.el
@@ -12,13 +12,11 @@
  '(haskell-program-name "ghci")
  '(haskell-stylish-on-save t)
  '(haskell-tags-on-save t)
- '(purescript-mode-hook (quote (turn-on-eldoc-mode turn-on-purescript-indentation))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(purescript-mode-hook (quote (turn-on-eldoc-mode turn-on-purescript-indentation)))
+ '(js2-basic-offset 2)
+ '(package-selected-packages
+   '(auto-virtualenv material-theme py-isort eslint-fix prettier-js js2-mode web-mode python solarized-theme)))
+
 
 (put 'downcase-region 'disabled nil)
 
