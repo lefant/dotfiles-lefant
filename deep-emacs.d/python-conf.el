d2a3fffedfc76d693aa54132bd40354345ca4c7c
Fabian Linzberger <e@lefant.net>
2021-03-12 09:53:55 +0100

    disable run-python running jupyter, add example for running ipython in docker

--- deep-emacs.d/python-conf.el
+++ deep-emacs.d/python-conf.el
@@ -1,10 +1,17 @@
 (require 'python)
 
-(setq python-shell-interpreter "jupyter"
-      python-shell-interpreter-args "console --simple-prompt"
-      python-shell-prompt-detect-failure-warning nil)
-(add-to-list 'python-shell-completion-native-disabled-interpreters
-             "jupyter")
+
+;; (setq python-shell-interpreter "run_all-libs-and-services_local_ipython.sh"
+;;       python-shell-interpreter-args ""
+;;       python-shell-prompt-detect-failure-warning nil)
+;; (add-to-list 'python-shell-completion-native-disabled-interpreters
+;;              "run_all-libs-and-services_local_ipython.sh")
+
+;; (setq python-shell-interpreter "jupyter"
+;;       python-shell-interpreter-args "console --simple-prompt"
+;;       python-shell-prompt-detect-failure-warning nil)
+;; (add-to-list 'python-shell-completion-native-disabled-interpreters
+;;              "jupyter")
 
 (require 'auto-virtualenv)
 (add-hook 'python-mode-hook 'auto-virtualenv-set-virtualenv)
