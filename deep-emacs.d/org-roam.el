7d6b2d6ce8f88ba051c1b6a4e827ff27e2f547dd
Fabian Linzberger <e@lefant.net>
2023-01-19 18:28:09 +0100

    catchup org-roam config

--- deep-emacs.d/org-roam.el
+++ deep-emacs.d/org-roam.el
@@ -1,9 +1,10 @@
 (use-package org-roam
   :custom
   ;;(org-roam-directory "/Users/lefant/git/lefant/notes")
-  (org-roam-directory "/Users/lefant/Google Drive/My Drive/logseq/notes")
+  (org-roam-directory (file-truename "~/Google Drive/My Drive/logseq/notes"))
   (org-roam-dailies-directory "journals/")
   (org-roam-capture-templates
    '(("d" "default" plain
-      #'org-roam-capture--get-point "%?"
-      :file-name "pages/${slug}" :head "#+title: ${title}\n" :unnarrowed t))))
+      "%?" :target
+      (file+head "pages/${slug}.org" "#+title: ${title}\n")
+      :unnarrowed t))))
